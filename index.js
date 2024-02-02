908ed351aec72229e17c94ffb81fc033846616f2
thesunny-dev <59652037+thesunny-dev@users.noreply.github.com>
2024-02-02 16:15:06 +0900

    beforeinstallprompt-설치하기 버튼

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const myImage = document.getElementsByClassName("img");
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   if (!Notification) {
-    alert('Notification not supported');
+    alert("Notification not supported");
     return;
   }
 });
 var myIndex = 0;
 
-function changeImage() {
+const changeImage = () => {
   var i;
   for (i = 0; i < myImage.length; i++) {
-    myImage[i].style.display = "none";  
+    myImage[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > myImage.length) {myIndex = 1}    
-  myImage[myIndex-1].style.display = "block";  
-  setTimeout(changeImage, 3000);    
-}
+  if (myIndex > myImage.length) {
+    myIndex = 1;
+  }
+  myImage[myIndex - 1].style.display = "block";
+  setTimeout(changeImage, 3000);
+};
 
-function init(){
-  myImage[0].style.display = "block";  
+const init = () => {
+  myImage[0].style.display = "block";
   for (i = 0; i < myImage.length; i++) {
-    myImage[i].style.display = "none";  
+    myImage[i].style.display = "none";
   }
   changeImage();
-}
+};
 
-
-function notif(bodis) {
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
+//////////////////////////////////////////////////
+const notif = (bodis) => {
+  if (Notification.permission !== "granted") Notification.requestPermission();
   else {
-    var notification = new Notification('Notification Title', {
-      icon: 'url/to/icon.png',
+    var notification = new Notification("Notification Title", {
+      icon: "url/to/icon.png",
       body: bodis,
     });
 
@@ -40,7 +41,33 @@ function notif(bodis) {
       window.open("window/open/on/notification/click");
     };
   }
-}
+};
 
+//////////////////////////////////////////////////
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  console.log("👍", "beforeinstallprompt", event);
+  window.deferredPrompt = event;
+});
+
+const installApp = async () => {
+  console.log("👍", "butInstall-clicked");
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    console.log("The deferred prompt isn't available.");
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log("👍", "userChoice", result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+};
+
+//////////////////////////////////////////////////
 init();
-
